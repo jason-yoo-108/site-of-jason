76ac3d657c4b776d91d22262952231df5d287e8f
Jason Yoo <jasonyoo0116@gmail.com>
2018-03-14 23:30:38 -0700

    finished experiences and projects

--- src/components/contents/Experiences.js
+++ src/components/contents/Experiences.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Jumbotron, Row, Col, Carousel } from 'react-bootstrap/lib';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/Carousel.css';
-import imageSAP from '../../images/mind_fulfilled.png';
-import imageHoot from '../../images/zenful_bot.png';
+import imageSAP from '../../images/sap.jpg';
+import imageHoot from '../../images/hootsuite.jpg';
 
 export class Experiences extends Component {
   constructor(props) {
@@ -11,28 +11,70 @@ export class Experiences extends Component {
   }
   //style={{marginLeft: 'auto', marginRight: 'auto'}}
   render() {
-    const fitParent = { width: '100%', height: '100%', borderRadius: 12 };
+    const linkSAP = 'https://www.sap.com/index.html';
+    const linkHoot = 'https://hootsuite.com/';
     return (
       <Jumbotron style={this.props.jumboStyle}>
         <h1> Experiences</h1>
-        <Carousel interval="10000">
+        <Carousel interval={10000}>
           <Carousel.Item>
             <Row style={{ margin: 0 }}>
               <Col xs={12} sm={6} style={{ padding: 24 }}>
-                <img src={imageSAP} />
+                <a href={linkSAP} target="_blank">
+                  <img src={imageSAP} />
+                </a>
               </Col>
               <Col xs={12} sm={6} style={{ padding: 24 }}>
-                <h3>SAP DevOps Engineer Internship</h3>
+                <h3>SAP DevOps Engineer Co-op</h3>
+                <h5>(Sep 2017 - Apr 2018)</h5>
+                <div style={{ textAlign: 'left' }}>
+                  <p>Here are some cool things that I've used:</p>
+                  <ul>
+                    <li>Languages: Ruby, Shell, Go</li>
+                    <li>
+                      Technologies: Capistrano, Chef, Git, Jenkins, Passenger,
+                      Ruby on Rails, Watir
+                    </li>
+                  </ul>
+                  <p>Here is a brief highlight of my work:</p>
+                  <ul>
+                    <li>Zero downtime restart during SAP Jam's deployment</li>
+                    <li>
+                      Server diagnostics such as checks for latency and dangling
+                      file pointers
+                    </li>
+                    <li>
+                      Deployment utilities such as rolling server warmup and
+                      selective deployment
+                    </li>
+                    <li>Cloud provisioning and package upgrades</li>
+                    <li>Test automation for realtime features</li>
+                  </ul>
+                </div>
               </Col>
             </Row>
           </Carousel.Item>
           <Carousel.Item>
             <Row style={{ margin: 0 }}>
               <Col xs={12} sm={6} style={{ padding: 24 }}>
-                <img src={imageHoot} />
+                <a href={linkHoot} target="_blank">
+                  <img src={imageHoot} />
+                </a>
               </Col>
               <Col xs={12} sm={6} style={{ padding: 24 }}>
-                <h3>Hootsuite Production Ops & Delivery Internship</h3>
+                <h3>Hootsuite Production Ops & Delivery Co-op</h3>
+                <h5>(May 2018 - Aug 2018)</h5>
+                <div style={{ textAlign: 'left' }}>
+                  <p>Here are some cool things that I've used:</p>
+                  <ul>
+                    <li>Languages: TBD</li>
+                    <li>Technologies: TBD</li>
+                  </ul>
+                  <p>Here is a brief highlight of my work:</p>
+                  <ul>
+                    <li>Coming soon :)</li>
+                  </ul>
+                </div>
               </Col>
             </Row>
           </Carousel.Item>
--- src/components/contents/Projects.js
+++ src/components/contents/Projects.js
@@ -1,19 +1,88 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap/lib';
+import { Jumbotron, Row, Col, Carousel } from 'react-bootstrap/lib';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../css/Carousel.css';
+import imageMindFulfilled from '../../images/mind_fulfilled.png';
+import imageZenfulBot from '../../images/zenful_bot.png';
 
 export class Projects extends Component {
   constructor(props) {
     super(props);
   }
+  //style={{marginLeft: 'auto', marginRight: 'auto'}}
   render() {
+    const linkZenfulBot = 'https://www.messenger.com/t/zenfulbot';
+    const linkMindFulfilled = 'https://github.com/truebluejason/mind_fulfilled';
     return (
       <Jumbotron style={this.props.jumboStyle}>
-        <h1>Hello, I'm a different card.</h1>
-        <p>
-          The true person is Not anyone in particular; But, like the deep blue
-          color Of the limitless sky, It is everyone, everywhere in the world.
-        </p>
+        <h1> Projects</h1>
+        <Carousel interval={10000}>
+          <Carousel.Item>
+            <Row style={{ margin: 0 }}>
+              <Col xs={12} sm={6} style={{ padding: 24 }}>
+                <a href={linkZenfulBot} target="_blank">
+                  <img src={imageZenfulBot} />
+                </a>
+              </Col>
+              <Col xs={12} sm={6} style={{ padding: 24 }}>
+                <h3>Zenful Bot: A Facebook Messenger Bot</h3>
+                <div style={{ textAlign: 'left' }}>
+                  <p>
+                    Zenful Bot is a Facebook bot designed to help you have a
+                    positive outlook on life.
+                  </p>
+                  <p>Here are some of the project's features:</p>
+                  <ul>
+                    <li>
+                      Cloud-based thankfulness journal for changing one's
+                      thinking pattern
+                    </li>
+                    <li>
+                      Engaging content delivery with messages, buttons, and
+                      images
+                    </li>
+                    <li>
+                      Links to studies that show benefits of cultivating
+                      gratefulness
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row style={{ margin: 0 }}>
+              <Col xs={12} sm={6} style={{ padding: 24 }}>
+                <a href={linkMindFulfilled} target="_blank">
+                  <img src={imageMindFulfilled} />
+                </a>
+              </Col>
+              <Col xs={12} sm={6} style={{ padding: 24 }}>
+                <h3>Mind Fulfilled: A CLI Meditation App</h3>
+                <div style={{ textAlign: 'left' }}>
+                  <p>
+                    Mind Fulfilled is an interactive meditation app designed to
+                    help you be aware of your personal distraction triggers over
+                    time.
+                  </p>
+                  <p>Here are some of the project's features:</p>
+                  <ul>
+                    <li>
+                      Minimalist interface for recording your distractions
+                      realtime
+                    </li>
+                    <li>
+                      Customizable message to be displayed during meditation
+                      session
+                    </li>
+                    <li>Statistics on your past data</li>
+                    <li>Motivational daily streak system</li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+          </Carousel.Item>
+        </Carousel>
       </Jumbotron>
     );
   }
